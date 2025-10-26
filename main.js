33a79bf50f6434d95339515776d58b1c9d26021d
Joseph Brown <jbrown@theecsgroup.com>
2025-10-26 17:06:36 +0000

    a

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            test:'product-image',
+            url: 'https://www.google.com'
+
         }
     }
 })
